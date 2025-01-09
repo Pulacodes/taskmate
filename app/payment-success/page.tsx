bf2bb00164871a3bda72c197d92694564646f4ad
pulacodes <pulalepholisa@gamil.com>
2025-01-09 04:02:19 +0200

    fixed some errors

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,10 +1,12 @@
 export default function PaymentSuccess({
-  searchParams: { amount },
+  searchParams,
 }: {
-  searchParams: { amount: string };
+  searchParams: { amount?: string };
 }) {
+  const amount = searchParams?.amount || "0"; // Default to "0" if amount is undefined
+
   return (
-    <main className="max-w-6xl mx-auto p-10 text-white text-center  m-10 rounded-md bg-gradient-to-tr from-gray-700 to-purple-500">
+    <main className="max-w-6xl mx-auto p-10 text-white text-center m-10 rounded-md bg-gradient-to-tr from-gray-700 to-purple-500">
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
         <h2 className="text-2xl">You successfully sent</h2>
