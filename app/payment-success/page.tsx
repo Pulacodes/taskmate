038239de3117d9b309856380b55d24c966207025
pulacodes <pulalepholisa@gamil.com>
2025-01-09 04:07:32 +0200

    fixed some errors

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,9 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
 export default function PaymentSuccess({
   searchParams,
 }: {
-  searchParams: { amount?: string };
+  searchParams: { amount: string } | undefined;
 }) {
-  const amount = searchParams?.amount || "0"; // Default to "0" if amount is undefined
+  const amount = searchParams?.amount || "0"; // Fallback to "0" if undefined
 
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center m-10 rounded-md bg-gradient-to-tr from-gray-700 to-purple-500">
