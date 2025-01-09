d13d861ef4fcabf853bf73f28a1bc3d36aa7f059
pulacodes <pulalepholisa@gamil.com>
2025-01-09 04:15:01 +0200

    fixed some errors

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,11 +1,17 @@
-
+import { useEffect, useState } from "react";
 
 export default function PaymentSuccess({
   searchParams,
 }: {
-  searchParams: { amount: string } | undefined;
+  searchParams: { amount: string };
 }) {
-  const amount = searchParams?.amount || "0"; // Fallback to "0" if undefined
+  const [amount, setAmount] = useState<string>("0");
+
+  useEffect(() => {
+    if (searchParams?.amount) {
+      setAmount(searchParams.amount);
+    }
+  }, [searchParams]);
 
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center m-10 rounded-md bg-gradient-to-tr from-gray-700 to-purple-500">
